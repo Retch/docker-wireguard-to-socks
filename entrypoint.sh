b7af873cdf62cc806ac808e2c3c11b65de751300
Retch <retch100@gmail.com>
2025-09-26 16:23:53 +0200

    improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,17 @@
 
 set -e
 
-DROUTE=$(ip route | grep default | awk '{print $3}')
-HOMENET=192.168.0.0/16
-HOMENET2=10.0.0.0/8
-HOMENET3=172.16.0.0/12
-ip route add $HOMENET3 via $DROUTE
-ip route add $HOMENET2 via $DROUTE
-ip route add $HOMENET via $DROUTE
-iptables -I OUTPUT -d $HOMENET -j ACCEPT
-iptables -A OUTPUT -d $HOMENET2 -j ACCEPT
-iptables -A OUTPUT -d $HOMENET3 -j ACCEPT
-
 oldip=$(curl -s ifconfig.me)
 
-ifname=$(basename $(ls -1 /etc/wireguard/*.conf | head -1) .conf)
+ifname=$(basename $(ls -1 /configdir/*.conf | head -1) .conf)
 
-echo "wg-quick up {$ifname}"
-wg-quick up /etc/wireguard/$ifname.conf
+cp /configdir/${ifname}.conf /etc/wireguard/wg99.conf
+
+echo "Using wireguard config ${ifname}"
+wg-quick up wg99
 
 echo "Regular IP: ${oldip}"
 
+sleep 3 && newip=$(curl -s ifconfig.me) && \
+  echo "Wireguard IP: ${newip}" && \
+  sockd -f /etc/sockd.conf
